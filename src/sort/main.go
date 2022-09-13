9620c25c8eb0a0c86dc7636102544edc7bdf8253
chao <22640673+chao7150@users.noreply.github.com>
2022-09-14 01:37:27 +0900

    sort

--- src/sort/main.go
+++ src/sort/main.go
@@ -82,18 +82,200 @@ func merge(a []int, b []int) []int {
 
 type Heap []int
 
-func (h *Heap) align(idx int) {
+func (h *Heap) alignUpward(idx int) {
 	val := (*h)[idx]
 	parentIdx := (idx - 1) / 2
 	parentVal := (*h)[parentIdx]
 	if val < parentVal {
 		(*h)[idx] = parentVal
 		(*h)[parentIdx] = val
-		h.align(parentIdx)
+		h.alignUpward(parentIdx)
 	}
+}
+
+func (h *Heap) swap(idx1 int, idx2 int) {
+	tmp := (*h)[idx1]
+	(*h)[idx1] = (*h)[idx2]
+	(*h)[idx2] = tmp
 }
 
 func (h *Heap) push(i int) {
 	*h = append(*h, i)
-	h.align(len(*h) - 1)
+	h.alignUpward(len(*h) - 1)
+}
+
+func (h *Heap) alignDownward(idx int) {
+	if len(*h) <= idx*2+1 {
+		// 子が存在しない
+		// 孫も存在しない
+		return
+
+	}
+	if len(*h) == idx*2+2 {
+		// 子が1つだけ存在する
+		if (*h)[idx*2+1] < (*h)[idx] {
+			h.swap(idx, idx*2+1)
+		}
+		// 孫は存在しない
+		return
+	}
+	// 子が2つ存在する
+	childIdxA := idx*2 + 1
+	childA := (*h)[childIdxA]
+	childIdxB := idx*2 + 2
+	childB := (*h)[childIdxB]
+	if childA <= childB {
+		if childA < (*h)[idx] {
+			h.swap(idx, childIdxA)
+			h.alignDownward(childIdxA)
+		}
+		return
+	}
+	if childB < (*h)[idx] {
+		h.swap(idx, childIdxB)
+		h.alignDownward(childIdxB)
+	}
+}
+
+func (h *Heap) DeleteMin() int {
+	res := (*h)[0]
+	(*h)[0] = (*h)[len(*h)-1]
+	*h = (*h)[:len(*h)-1]
+	h.alignDownward(0)
+	return res
+}
+
+func HeapSort(s []int) []int {
+	h := Heap{}
+	for _, v := range s {
+		h.push(v)
+	}
+	o := []int{}
+	for i := 0; i < len(s); i++ {
+		o = append(o, h.DeleteMin())
+	}
+	return o
+}
+
+func (h *Heap) Heapify() {
+	for i := (len(*h) - 2) / 2; 0 <= i; i-- {
+
+	}
+}
+
+func HeapSortInplace(s []int) {
+
+}
+
+func BucketSort(a []int) []int {
+	bucket := make([]int, 10)
+	for _, v := range a {
+		bucket[v] = v
+	}
+	out := []int{}
+	for _, v := range bucket {
+		if v != 0 {
+			out = append(out, v)
+		}
+	}
+	return out
+}
+
+func DupBucketSort(a []int) []int {
+	occurrenceMap := make([]int, 9)
+	for _, v := range a {
+		occurrenceMap[v]++
+	}
+	offSet := make([]int, 9)
+	for i := range occurrenceMap {
+		if i == 0 {
+			break
+		}
+		offSet[i] = offSet[i-1] + occurrenceMap[i-1]
+	}
+	bucket := make([]int, len(a))
+	for _, v := range a {
+		bucket[v] = v
+	}
+	out := []int{}
+	for _, v := range bucket {
+		if v != 0 {
+			out = append(out, v)
+		}
+	}
+	return out
+}
+
+type IntAndSortKey struct {
+	sortKey int
+	value   int
+}
+
+func DupBucketSortWithKey(a []IntAndSortKey) []IntAndSortKey {
+	occurrenceMap := make([]int, 10)
+	for _, v := range a {
+		occurrenceMap[v.sortKey]++
+	}
+	offSet := make([]int, 10)
+	for i := range occurrenceMap {
+		if i == 0 {
+			continue
+		}
+		offSet[i] = offSet[i-1] + occurrenceMap[i-1]
+	}
+	bucket := make([]IntAndSortKey, len(a))
+	for _, v := range a {
+		bucket[offSet[v.sortKey]] = v
+		offSet[v.sortKey]++
+	}
+	out := []IntAndSortKey{}
+	for _, v := range bucket {
+		if v.sortKey != 0 {
+			out = append(out, v)
+		}
+	}
+	return out
+}
+
+func PowInt(target int, pow int) int {
+	if pow == 0 {
+		return 1
+	}
+	ret := target
+	for i := 0; i < pow-1; i++ {
+		ret *= ret
+	}
+	return ret
+}
+
+func GetDigit(target int, pos int) int {
+	if target < 10 {
+		return target
+	}
+	if pos == 1 {
+		return target % 10
+	}
+	return GetDigit(target/PowInt(10, pos-1), 1)
+}
+
+func BucketSortByDigit(a []int, digit int) []int {
+	intAndSortKeys := []IntAndSortKey{}
+	for _, v := range a {
+		sortKey := GetDigit(v, digit)
+		intAndSortKeys = append(intAndSortKeys, IntAndSortKey{sortKey, v})
+	}
+	sorted := DupBucketSortWithKey(intAndSortKeys)
+	res := []int{}
+	for _, v := range sorted {
+		res = append(res, v.value)
+	}
+	return res
+}
+
+func RadixSort4digits(a []int) []int {
+	res := a
+	for i := 0; i < 4; i++ {
+		res = BucketSortByDigit(res, i+1)
+	}
+	return res
 }
--- src/sort/main_test.go
+++ src/sort/main_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestSort(t *testing.T) {
 	a := []int{8, 4, 5, 7, 6, 1, 2, 9, 3}
-	b := MergeSort(a)
+	b := HeapSort(a)
 	c := 0
 	for i, v := range b {
 		if v < c {
@@ -27,9 +27,9 @@ func TestMerge(t *testing.T) {
 	}
 }
 
-func TestAlign(t *testing.T) {
+func TestAlignUpward(t *testing.T) {
 	h := Heap{1, 3, 6, 4, 8, 7, 10, 5, 9, 11, 2}
-	h.align(10)
+	h.alignUpward(10)
 	res := []int{1, 2, 6, 4, 3, 7, 10, 5, 9, 11, 8}
 	for i, v := range h {
 		if v != res[i] {
@@ -49,5 +49,154 @@ func TestPush(t *testing.T) {
 		if v != res[i] {
 			t.Fail()
 		}
+	}
+}
+
+func TestHeapSwap(t *testing.T) {
+	h := Heap{1, 2}
+	h.swap(0, 1)
+	res := []int{2, 1}
+	AssertSliceEqual(t, h, res)
+}
+
+func AssertSliceEqual(t *testing.T, s []int, res []int) {
+	for i, v := range s {
+		if v != res[i] {
+			t.Fatalf(`expected: %v, given: %v`, res, s)
+		}
+	}
+}
+
+func TestHeapAlignDownward(t *testing.T) {
+	h := Heap{0}
+	h.alignDownward(0)
+	res := []int{0}
+	AssertSliceEqual(t, h, res)
+
+	h = Heap{1, 0}
+	h.alignDownward(0)
+	res = []int{0, 1}
+	AssertSliceEqual(t, h, res)
+
+	h = Heap{0, 1}
+	h.alignDownward(0)
+	res = []int{0, 1}
+	AssertSliceEqual(t, h, res)
+
+	h = Heap{2, 1, 0}
+	h.alignDownward(0)
+	res = []int{0, 1, 2}
+	AssertSliceEqual(t, h, res)
+
+	h = Heap{3, 2, 4, 0}
+	h.alignDownward(0)
+	res = []int{2, 0, 4, 3}
+	AssertSliceEqual(t, h, res)
+
+	h = Heap{9, 3, 6, 6, 8, 10, 12, 10, 13, 11}
+	h.alignDownward(0)
+	res = []int{3, 6, 6, 9, 8, 10, 12, 10, 13, 11}
+	AssertSliceEqual(t, h, res)
+}
+
+func TestHeapDeleteMin(t *testing.T) {
+	h := Heap{1, 3, 6, 6, 8, 10, 12, 10, 13, 11, 9}
+	min := h.DeleteMin()
+	res := []int{3, 6, 6, 9, 8, 10, 12, 10, 13, 11}
+	AssertSliceEqual(t, h, res)
+	if min != 1 {
+		t.Fail()
+	}
+	min = h.DeleteMin()
+	res = []int{6, 8, 6, 9, 11, 10, 12, 10, 13}
+	AssertSliceEqual(t, h, res)
+	if min != 3 {
+		t.Fail()
+	}
+}
+
+func TestBucketSort(t *testing.T) {
+	a := []int{8, 4, 5, 7, 6, 1, 2, 9, 3}
+	b := BucketSort(a)
+	c := 0
+	for i, v := range b {
+		if v < c {
+			t.Fatalf(`%v th value %v is smaller than previous value %v`, i, v, c)
+		}
+		c = v
+	}
+}
+
+func TestDupBucketSort(t *testing.T) {
+	a := []int{3, 2, 2, 1, 2, 2, 1, 3, 3, 1, 2, 3}
+	b := DupBucketSort(a)
+	c := 0
+	for i, v := range b {
+		if v < c {
+			t.Fatalf(`%v th value %v is smaller than previous value %v`, i, v, c)
+		}
+		c = v
+	}
+}
+
+func TestDupBucketSortWithKey(t *testing.T) {
+	a := []IntAndSortKey{{4, 24}, {4, 14}, {1, 11}}
+	b := DupBucketSortWithKey(a)
+	res := []int{11, 24, 14}
+	for i, v := range b {
+		if res[i] != v.value {
+			t.Fail()
+		}
+	}
+	a = []IntAndSortKey{{1, 251}, {1, 151}, {4, 334}, {4, 114}, {4, 114}, {3, 513}, {5, 365}, {3, 363}, {1, 1}, {1, 1001}, {2, 42}}
+	b = DupBucketSortWithKey(a)
+	res = []int{251, 151, 1, 1001, 42, 513, 363, 334, 114, 114, 365}
+	for i, v := range b {
+		if res[i] != v.value {
+			t.Fail()
+		}
+	}
+}
+
+func TestPowInt(t *testing.T) {
+	if PowInt(10, 0) != 1 {
+		t.Fail()
+	}
+	if PowInt(10, 1) != 10 {
+		t.Fail()
+	}
+	if PowInt(10, 2) != 100 {
+		t.Fail()
+	}
+}
+
+func TestGetDigit(t *testing.T) {
+	res := GetDigit(12345, 1)
+	if res != 5 {
+		t.Fail()
+	}
+	res = GetDigit(12345, 2)
+	if res != 4 {
+		t.Fail()
+	}
+	res = GetDigit(12345, 3)
+	if res != 3 {
+		t.Fail()
+	}
+	res = GetDigit(12345, 6)
+	if res != 0 {
+		t.Fail()
+	}
+}
+
+func TestRadixSort(t *testing.T) {
+	a := []int{251, 151, 334, 114, 114, 513, 365, 363, 1, 1001, 42}
+	b := RadixSort4digits(a)
+	c := 0
+	for i, v := range b {
+		if v < c {
+			t.Fatalf(`%v th value %v is smaller than previous value %v`, i, v, c)
+		}
+		c = v
 	}
 }
